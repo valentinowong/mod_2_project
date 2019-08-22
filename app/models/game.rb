c14df1e2777d9d5decd22e87faa650de0d27b0a4
Meghannwalsh <meghann.mccool.walsh@gmail.com>
2019-08-22 15:48:20 -0400

    can make as many missions as they want now

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,14 @@
 class Game < ApplicationRecord
     has_many :missions
     has_many :game_rooms
-    accepts_nested_attributes_for :missions
+
+    def missions_attributes=(mission_attributes)
+    mission_attributes.values.each do |mission_attribute|
+      
+      if mission_attribute.values.all? { |attribute| !attribute.empty? }
+        mission = Mission.create(mission_attribute)
+        self.missions << mission
+      end 
+    end
+  end
 end
